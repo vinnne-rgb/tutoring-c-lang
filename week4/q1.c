f2a80a53567a0f254ebfd5b70b16ffa8230095d7
vinnne-rgb <vinnne75@gmail.com>
2026-05-04 14:19:43 +0900

    feat : 첫번째 문제

--- week4/q1.c
+++ week4/q1.c
@@ -6,10 +6,19 @@
 -1억~1억사이의 정수가 주어진다.
 0이 아니면 입력된 정수를 출력하고, 0이 입력되면 출력을 중단하라.
 */
-int main() {
+int main(void) {
     // 파일 입출력 설정
     freopen("q1in.txt", "r", stdin);
     freopen("q1out.txt", "w", stdout);
 
+   int x;
+
+while (1)
+    {
+	scanf_s("%d", &x);
+	if (x == 0) break;
+	printf("%d\n", x);
+    }
+    
     return 0;
 }

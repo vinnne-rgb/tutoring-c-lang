98bb88f41c408227c526ea20d95065f1e1a42c28
vinnne-rgb <vinnne75@gmail.com>
2026-05-04 14:46:08 +0900

    feat : 최종 확인부탁드립니다!

--- week4/q3.c
+++ week4/q3.c
@@ -6,16 +6,17 @@
 1~100 사이의 정수n이 주어진다.
 정수 n의 카운트다운을 출력하라.
 */
-int main(void) {
+int main(void)
+{
     // 파일 입출력 설정
     freopen("q3in.txt", "r", stdin);
     freopen("q3out.txt", "w", stdout);
 
     int n;
-scanf("%d", &n);
-for (int i = n; i >= 1; i--)
+    scanf("%d", &n);
+    for (int i = n; i >= 1; i--)
     {
-	printf("%d\n", i);
+        printf("%d\n", i);
     }
 
     return 0;
--- week4/q4.c
+++ week4/q4.c
@@ -6,16 +6,17 @@
 'a' ~ 'z' 영문자 1개가 주어진다.
 주어진 영문자까지의 알파벳을 순서대로 출력하라.
 */
-int main(void) {
+int main(void)
+{
     // 파일 입출력 설정
     freopen("q4in.txt", "r", stdin);
     freopen("q4out.txt", "w", stdout);
 
     char c;
-scanf("%c", &c);
-for (char ch = 'a'; ch <= c; ch++)
+    scanf("%c", &c);
+    for (char ch = 'a'; ch <= c; ch++)
     {
-	printf("%c ", ch);
+        printf("%c ", ch);
     }
 
     return 0;

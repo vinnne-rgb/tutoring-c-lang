95dd69f6e4b0485bd2090caaa91c8972e279de18
vinnne-rgb <vinnne75@gmail.com>
2026-05-04 14:26:47 +0900

    feat : 2~5

--- week4/q2.c
+++ week4/q2.c
@@ -7,10 +7,20 @@
 이후 n개의 -1억~1억 사이의 정수가 주어진다.
 n개의 입력된 정수를 순서대로 출력하라.
 */
-int main() {
+int main(void) {
     // 파일 입출력 설정
     freopen("q2in.txt", "r", stdin);
     freopen("q2out.txt", "w", stdout);
+
+    int n;
+scanf_s("%d", &n);
+for (int i = 0; i < n; i++)
+
+    {
+	  int x;
+	  scanf_s("%d", &x);
+	  printf("%d\n", x);
+    }
 
     return 0;
 }

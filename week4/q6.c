87601ff974b662fc155b7321b2d9abe58a497fde
vinnne-rgb <vinnne75@gmail.com>
2026-05-04 15:44:34 +0900

    feat : q6 확인 부탁드립니다 gyb

--- week4/q6.c
+++ week4/q6.c
@@ -6,10 +6,19 @@
 1~9 범위의 정수 n이 주어진다.
 구구단 N단을 출력하라.
 */
-int main() {
+int main(void) {
     // 파일 입출력 설정
     freopen("q6in.txt", "r", stdin);
     freopen("q6out.txt", "w", stdout);
 
+    for (int n = 1; n <= 9; n++)
+    {
+        printf("===%d단===\n", n);
+        for (int i = 1; i <= 9; i++)
+        {
+            printf("%d x %d = %d\n", n, i, n * i);
+        }
+        printf("\n");
+    }
     return 0;
 }

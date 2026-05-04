390b71b581b484506047979defa6d2399010a2f8
eoehd1ek <eoehd2ek@gmail.com>
2026-05-04 13:50:34 +0900

    feat: 4차시 형성 평가 추가

---
+++ week4/q6.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#define _CRT_SECURE_NO_WARNINGS
+
+/*
+반복문을 사용하여 구구단 프로그램을 완성한다.
+1~9 범위의 정수 n이 주어진다.
+구구단 N단을 출력하라.
+*/
+int main() {
+    // 파일 입출력 설정
+    freopen("q6in.txt", "r", stdin);
+    freopen("q6out.txt", "w", stdout);
+
+    return 0;
+}

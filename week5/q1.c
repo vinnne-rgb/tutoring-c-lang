a45a44a1497c67c7e602b93eeb38882c987de0cb
eoehd1ek <eoehd2ek@gmail.com>
2026-05-07 02:50:03 +0900

    feat: 5주차 퀴즈 추가

---
+++ week5/q1.c
@@ -0,0 +1,22 @@
+
+#include <stdio.h>
+#define _CRT_SECURE_NO_WARNINGS
+
+/*
+10이하의 정수 n이 주어진다.
+1~3000 사이의 정수 year가 n개 주어진다.
+year가 윤년이면 1, 아니라면 0을 반환하는 함수 leap_year을 구현한다.
+이 작업을 n번 반복하면서 함수 leap_year을 호출하여 결과를 출력하라.
+*/
+
+int leap_year(int year) {
+    // TODO: 구현하기
+}
+
+int main() {
+    // 파일 입출력 설정
+    freopen("q1in.txt", "r", stdin);
+    freopen("q1out.txt", "w", stdout);
+
+    return 0;
+}
---
+++ week5/q2.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define _CRT_SECURE_NO_WARNINGS
+
+/*
+10이하의 정수 n이 주어진다.
+표준라이브러리 stdlib.h의 랜덤 함수 rand()를 사용하여,
+1~100 사이의 값을 출력하는 함수 solution을 구현한다.
+이 작업을 n번 반복하라.
+미리 작성되어 있는 srand() 코드는 수정하지 않는다.
+*/
+
+void solution() {
+    // TODO: 구현하기
+}
+
+int main() {
+    // 파일 입출력 설정
+    freopen("q2in.txt", "r", stdin);
+    freopen("q2out.txt", "w", stdout);
+    srand(42);
+
+    // TODO: 구현하기
+
+    return 0;
+}
